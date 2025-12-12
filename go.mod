3686933cf042327580bd63d63520bdae98cee4a6
Yash2402 <yashsharma4583@gmail.com>
2025-12-12 22:00:42 +0530

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module jotacomputing/go-api
+
+go 1.24.10
+
+require github.com/edsrzf/mmap-go v1.2.0
+
+require golang.org/x/sys v0.33.0 // indirect
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+const queueFilePath = "/tmp/sex"
+const order = queue.Order{
+	order_id:   3, //uint64
+	price: 		12000, //uint64
+	timestamp: 	uint64(time.Now().UnixNano()),
+	user_id:  	1001,
+	shares_qty: 16,
+	symbol:    	0,
+	side: 		0,
+	order_type: 0,
+	status:    	0, // pending
+}
+	
+func main() {
+}
