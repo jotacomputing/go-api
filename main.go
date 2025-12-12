9e9e4444dea24c6be352ab5ca31aa1905d73a931
Yash2402 <yashsharma4583@gmail.com>
2025-12-12 22:03:52 +0530

    `sendOrder` single order function

--- main.go
+++ main.go
@@ -48,5 +48,35 @@ func initQueue() {
 	fmt.Printf("[INIT] File: %s_status (size: ~3.2 MB)\n", queueFilePath)
 }
 
+// sendOrder sends a single order
+func sendOrder(order ShmOrder) {
+	q, err := queue.OpenQueue(queueFilePath)
+	if err != nil {
+		log.Fatalf("Failed to open queue: %v", err)
+	}
+	defer q.Close()
+
+	// order := queue.Order{
+	// 	order_id:   3, //uint64
+	// 	price: 		12000, //uint64
+	// 	timestamp: 	uint64(time.Now().UnixNano()),
+	// 	user_id:  	1001,
+	// 	shares_qty: 16,
+	// 	symbol:    	0,
+	// 	side: 		0,
+	// 	order_type: 0,
+	// 	status:    	0, // pending
+	// }
+
+	if err := q.Enqueue(order); err != nil {
+		log.Fatalf("Failed to enqueue: %v", err)
+	}
+
+	fmt.Printf("[COMM] Single order sent successfully\n")
+	fmt.Printf("       OrderID: %d\n", order.OrderID)
+	fmt.Printf("       Symbol: %s\n", string(order.Symbol))
+	fmt.Printf("       Qty: %d @ %d\n", order.Quantity, order.Price)
+	fmt.Printf("       Queue depth: %d\n", q.Depth())
+}
 func main() {
 }
